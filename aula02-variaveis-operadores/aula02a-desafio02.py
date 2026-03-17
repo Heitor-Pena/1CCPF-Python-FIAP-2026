d5fec8abad01a2b6ab84cd846b4f4a4cb7ece352
heitorpena <heitorpenagomes@gmail.com>
2026-03-17 11:41:17 -0300

    aula 02

---
+++ aula02-variaveis-operadores/aula02a-desafio02.py
@@ -0,0 +1,5 @@
+print ("Quando você nasceu?")
+dia = input("Dia: ")
+mes = input("Mês: ")
+ano = input("Ano: ")
+print ("Você nasceu em:",dia,"/", mes,"/", ano)
---
+++ aula02-variaveis-operadores/aula02a-desafio03.py
@@ -0,0 +1,3 @@
+numero01 = input("Numero 1: ")
+numero02 = input("Numero 2: ")
+print (int(numero01) + int(numero02))
